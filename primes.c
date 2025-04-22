ac642146d2a735b1d2b704e611870ae00ded52d8
Destin <destinwong@gmail.com>
2025-04-22 13:10:44 -0700

    Implemented single thread

--- primes.c
+++ primes.c
@@ -1,6 +1,7 @@
+// testing if this shows
 #include <stdio.h>
+#include <math.h>
 #include <pthread.h>
-
 #include "primes.h"//implementing
 
 typedef struct { //example structure
@@ -30,15 +31,41 @@ static void example_helper_function(int n){
     return;
 }
 
+// max is the max number to test primes up to, verb indicates whether to print the primes
+// print the prime if verb != 0
 void primes_st(unsigned int max, unsigned int verb){
     //Put your code here.
     //example_helper_function(1000);
+    // outer loop: go over all numbers
+    
+    for(int tested_number = 2; tested_number < (max); tested_number++){
+        int isPrime = 1;
+        
+        // inner loop: test every number up to half to see if there are
+        // any valid factors. If not, it's a prime
+        for(int factor = 2; factor*factor <= tested_number; factor++){
+            
+            // if modulo gives zero, it is not a prime
+            // is a prime
+            if (tested_number % factor == 0){
+                isPrime = 0;  // not prime
+                break;
+            } 
+                
+        }
+        if (isPrime == 1){
+            printf("%d is a prime\n", tested_number);
+        }
+        
+    }
     return;
 }
 
 void primes_mt(unsigned int max, unsigned int threads,	\
 	       unsigned int verb){
+               
     //Put your code here.
     //example_helper_function(2000);
+    // mutex should be counter
     return;
 }
